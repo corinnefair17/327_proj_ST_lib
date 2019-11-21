1a7e9c705020c0b9b13f2c597a8e8a97acff0077
Corinne Fair <corinne.fair.17@cnu.edu>
2019-11-21 16:00:33 -0500

    skeleton for st_american_donutenthusiest (which is spelled wrong, by the way)

--- ST_American_DonutEnthusiest.cpp
+++ ST_American_DonutEnthusiest.cpp
@@ -5,6 +5,17 @@
  *      Author: corinne
  */
 
+#include "./includes/ST_American_DonutEnthusiest.h"
+#include "./includes/constants.h"
 
+ST_American_DonutEnthusiest::ST_American_DonutEnthusiest(int i):Smalltalk_American(AMERICAN_DE, i) {
 
+}
 
+ST_American_DonutEnthusiest::~ST_American_DonutEnthusiest(void) {
+
+}
+
+void ST_American_DonutEnthusiest::populatePhrases(void) {
+
+}
