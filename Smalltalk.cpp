90c859e4d8c350147026eef3ece9459a97d7a629
Corinne Fair <corinne.fair.17@cnu.edu>
2019-11-22 19:55:19 -0500

    wrote saysomething

--- Smalltalk.cpp
+++ Smalltalk.cpp
@@ -6,6 +6,8 @@
  */
 
 #include "./includes/Smalltalk.h"
+#include "./includes/constants.h"
+#include "./includes/Watch.h"
 
 // derived class will set Nationality, iPerson
 // iPerson is just a counter used to distinguish between objects of the same type
@@ -24,23 +26,68 @@ Smalltalk::~Smalltalk(void) {
 // for instance the following string comes from an American instance, the 10th iPerson and it is printing AMERICAN_PHRASE_2
 // AMERICAN 10:Why yes, I would like to supersize that
 std::string Smalltalk::saySomething() {
-	return "\0";
+	std::string result;
+	std::string heading = nationality + " " + std::to_string(iPerson) + ":";
+
+	if (nationality == AMERICAN) {
+		result += heading + AMERICAN_PHRASE_1 + "\n";
+		result += heading + AMERICAN_PHRASE_2 + "\n";
+		result += heading + AMERICAN_PHRASE_3 + "\n";
+		result += heading + AMERICAN_PHRASE_4 + "\n";
+		result += heading + AMERICAN_PHRASE_5 + "\n";
+		result += heading + AMERICAN_PHRASE_1 + "\n";
+	}
+	else if (nationality == AMERICAN_DE) {
+		result += heading + AMERICAN_PHRASE_1 + "\n";
+		result += heading + AMERICAN_PHRASE_2 + "\n";
+		result += heading + AMERICAN_PHRASE_3 + "\n";
+		result += heading + AMERICAN_PHRASE_4 + "\n";
+		result += heading + AMERICAN_PHRASE_5 + "\n";
+		result += heading + AMERICAN_DE_PHRASE_1 + "\n";
+		result += heading + AMERICAN_DE_PHRASE_2 + "\n";
+		result += heading + AMERICAN_DE_PHRASE_3 + "\n";
+		result += heading + AMERICAN_DE_PHRASE_4 + "\n";
+		result += heading + AMERICAN_DE_PHRASE_5 + "\n";
+		result += heading + AMERICAN_PHRASE_1 + "\n";
+	}
+	else if (nationality == BRIT) {
+		result += heading + BRIT_1 + "\n";
+		result += heading + BRIT_2 + "\n";
+		result += heading + BRIT_3 + "\n";
+		result += heading + BRIT_4 + "\n";
+		result += heading + BRIT_5 + "\n";
+		result += heading + BRIT_6 + "\n";
+		result += heading + BRIT_7 + "\n";
+		result += heading + BRIT_1 + "\n";
+	}
+
+	return result;
 }
 
 // returns the time (if pWatch contains a watch ) in the form of THE_CURRENT_TIME_IS: (from the actual watch object itself) and then the time
 // or I_DO_NOT_HAVE_A_WATCH string (if pWatch does not contain a watch)
 std::string Smalltalk::getTime() {
+	if (pWatch == 0) {
+		return I_DO_NOT_HAVE_A_WATCH;
+	}
 	return "\0";
 }
 
 // if this object has a watch it is taken away, otherwise an empty unique_ptr is returned
 // This transaction simulates giving away a watch
 std::unique_ptr<Watch> Smalltalk::takeWatch() {
+	if (pWatch == 0) {
+		// return std::unique_ptr<Watch> watch;
+	}
 	return nullptr;
 }
 
 // if already have a watch then return false and dont change pWatch pointer
 // otherwise accept watch (return true) and set this->pWatch=pWatch (use std::move)
 bool Smalltalk::giveWatch(std::unique_ptr<Watch> &pWatch) {
-	return false;
+	if (pWatch != 0) {
+		return false;
+	}
+	this->pWatch = std::move(pWatch);
+	return true;
 }
